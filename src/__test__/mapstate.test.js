4f878be6d36f811c96faf8650940e0af2e28c50e
Esa-Matti Suuronen <esa-matti@suuronen.org>
2016-10-09 13:35:39 +0000

    Add update test to mapState

--- src/__test__/mapstate.test.js
+++ src/__test__/mapstate.test.js
@@ -1,12 +1,9 @@
 import React from "react";
 import {createStore} from "redux";
-import {Provider} from "react-redux";
-import {get} from "lodash/fp";
-import renderer from "react-test-renderer";
 
 import {render} from "./helpers";
 
-import {leanReducer, connectLean, update, thunk} from "../lean";
+import {leanReducer, connectLean, update} from "../lean";
 
 test("can map state", () => {
     const store = createStore(leanReducer);
@@ -26,6 +23,8 @@ test("can map state", () => {
 
 
     const component = render(store, Connected);
+    expect(component.toJSON()).toMatchSnapshot();
+    store.dispatch(update({name: "updated"}));
     expect(component.toJSON()).toMatchSnapshot();
 
 
