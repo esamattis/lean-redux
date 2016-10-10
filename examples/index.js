bf26a4287b6e73044fd50aca64da5a1de0cd393a
Esa-Matti Suuronen <esa-matti@suuronen.org>
2016-10-10 20:48:02 +0000

    examples++

--- examples/index.js
+++ examples/index.js
@@ -156,6 +156,9 @@ var Main = ({gifsVisible, showGifs}) => (
 
             <h2>Random Gif List</h2>
             <p>
+                This is just a simpler version the Named Counters example. The
+                fact that the nested components are async doesnt' show up here
+                in anyway.
             </p>
             <Example source="RandomGifList.js" >
                 <RandomGifList scope={[GIF_SCOPE, "list"]} />
