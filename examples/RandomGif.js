c03e0696681fd9070e630b02231ab9828042d2e0
Esa-Matti Suuronen <esa-matti@suuronen.org>
2016-10-11 21:13:13 +0000

    examples++ [ci skip]

--- examples/RandomGif.js
+++ examples/RandomGif.js
@@ -29,10 +29,6 @@ RandomGif = connectLean({
         };
     },
 
-    setUrl(url) {
-        return {url};
-    },
-
     fetchGif() {
         this.setState({status: "fetching"});
         fetch("https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + this.props.tag)
