5b87d4a608264eba0d2c2f25625c8b978310f60f
Esa-Matti Suuronen <esa-matti@suuronen.org>
2016-10-11 19:18:15 +0000

    cleanup

--- examples/MultipleCounters.js
+++ examples/MultipleCounters.js
@@ -5,11 +5,9 @@ import Counter from "./Counter";
 var MultipleCounters = () => (
     <div>
         <div>
-            <div>Increments by 2</div>
             <Counter scope="multicounter1" amount={2} />
         </div>
         <div>
-            <div>Increments by 10</div>
             <Counter scope="multicounter2" amount={10} />
         </div>
     </div>
