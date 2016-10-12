d802de4466780289eea2979baca82fb523b303f2
Esa-Matti Suuronen <esa-matti@suuronen.org>
2016-10-12 17:47:17 +0000

    mapstate test: defaultProps

--- tests/mapstate.test.js
+++ tests/mapstate.test.js
@@ -28,5 +28,33 @@ describe("map state", () => {
         render(store, Hello);
         expect(spy).toHaveBeenCalledWith("from initial");
     });
+
+    test("defaultProps in ownProps", () => {
+        const propSpy = jest.fn();
+        const stateSpy = jest.fn();
+        const store = createStore(leanReducer);
+        let Hello = ({name}) => {
+            return <div>Hello {name}</div>;
+        };
+
+        Hello = connectLean({
+            scope: "ascope",
+            getInitialState() {
+                return {name: "from initial"};
+            },
+            defaultProps: {
+                aProp: "default prop",
+            },
+            mapState(state, ownProps) {
+                propSpy(ownProps.aProp);
+                stateSpy(state.aProp);
+            },
+        })(Hello);
+
+        render(store, Hello);
+        expect(propSpy).toHaveBeenCalledWith("default prop");
+        expect(stateSpy).toHaveBeenCalledWith(undefined);
+    });
+
 });
 
