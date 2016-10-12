469a92b350d1126d3981dcd9c32b7327e4777d38
Esa-Matti Suuronen <esa-matti@suuronen.org>
2016-10-12 17:29:38 +0000

    Add parent props test

--- tests/rendering.test.js
+++ tests/rendering.test.js
@@ -19,6 +19,24 @@ test("renders from initial state", () => {
         },
     })(Hello);
 
-    const component = render(store, Hello);
+    const {component} = render(store, Hello);
+    expect(component.toJSON()).toMatchSnapshot();
+});
+
+test("props from parent don't override state", () => {
+    const store = createStore(leanReducer);
+    let Hello = ({name}) => {
+        return <div>Hello {name}</div>;
+    };
+
+    Hello = connectLean({
+        scope: "ascope",
+        getInitialState() {
+            return {name: "esa"};
+        },
+    })(Hello);
+
+    const {component, setProps} = render(store, Hello);
+    setProps({name: "from parent"});
     expect(component.toJSON()).toMatchSnapshot();
 });
