baaec9271d61e8d9e65c050fba71fa83ebbb2147
Esa-Matti Suuronen <esa-matti@suuronen.org>
2016-10-12 18:16:45 +0000

    Add scope test

---
+++ tests/scope.test.js
@@ -0,0 +1,35 @@
+import React from "react";
+import {createStore} from "redux";
+
+import {leanReducer, connectLean} from "../src/lean";
+
+import {render} from "./helpers";
+
+describe("scope", () => {
+
+    test("is used to set the location in redux state", () => {
+        const store = createStore(leanReducer);
+        let handler = null;
+
+        let Hello = ({name, setName}) => {
+            handler = setName;
+            return <div>Hello {name}</div>;
+        };
+
+        Hello = connectLean({
+            scope: "ascope",
+            getInitialState() {
+                return {name: "from initial"};
+            },
+            setName() {
+                this.setState({name: "from handler"});
+            },
+        })(Hello);
+
+        render(store, Hello);
+        handler();
+        expect(store.getState()).toMatchSnapshot();
+    });
+
+});
+
