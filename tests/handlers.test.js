7b4bc444da840d2e33581400150f83f9495df79e
Esa-Matti Suuronen <esa-matti@suuronen.org>
2016-10-12 18:07:00 +0000

    test handlers: has props from defaultProps

--- tests/handlers.test.js
+++ tests/handlers.test.js
@@ -7,6 +7,7 @@ import {leanReducer, connectLean} from "../src/lean";
 import {render} from "./helpers";
 
 describe("handler context", () => {
+
     test("has state from initial state", () => {
         const spy = jest.fn();
         const store = createStore(leanReducer);
@@ -89,5 +90,33 @@ describe("handler context", () => {
         expect(spy).toHaveBeenLastCalledWith("from handler");
     });
 
+    test("has props from defaultProps", () => {
+        const spy = jest.fn();
+        const store = createStore(leanReducer);
+        let handler = null;
+
+        let Hello = ({name, setName}) => {
+            handler = setName;
+            return <div>Hello {name}</div>;
+        };
+
+        Hello = connectLean({
+            scope: "ascope",
+            getInitialState() {
+                return {name: "from initial"};
+            },
+            defaultProps: {
+                aProp: "default prop",
+            },
+            setName() {
+                spy(this.props.aProp);
+                this.setState({name: "from handler"});
+            },
+        })(Hello);
+
+        render(store, Hello);
+        handler();
+        expect(spy).toHaveBeenLastCalledWith("default prop");
+    });
 });
 
